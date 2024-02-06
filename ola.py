35812c420b75b58f8f9a09e386b948dab9e7fbb8
Luís Eduardo <158162672+sagxl@users.noreply.github.com>
2024-02-06 09:28:32 -0300

    Update ola.py

--- ola.py
+++ ola.py
@@ -58,3 +58,87 @@ print(f'valor a pagar e {valor_final}')
 #git add ola.py
 #git commit -m "coamndos basicos "
 #git push
+
+#1. Faça um programa que imprima o seu nome.
+
+nome = input('digite seu nome ')
+print(f'ola {nome}. bom dia (a)!')
+#ou
+
+print('luis eduardo')
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+#2. Faça um programa que imprima o produto dos valores 30 e 27.
+
+print(30*27)
+
+#----------------------------------------------------------------------------------------------------------------------------
+#3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
+ 
+n1 = 6
+n2 = 8
+n3 = 12
+soma = (n1+n2+n3) / 3
+print(f'{soma}')
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+#4. Faça um programa que leia e imprima um número inteiro.
+
+numero = int(input('digite um numero:'))
+print(f'{numero}')
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+#5. Faça um programa que leia dois números reais e os imprima.
+
+nn1 = float(input('digite um primeiro numero:'))
+nn2 = float(input('digite o segundo numero:'))
+ 
+#print(f'{nn1}\t{nn2}') 
+print(f'{nn1}') 
+print(f'{nn2}') 
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+#6. Faça um programa que leia um número inteiro e imprima o seu
+#   antecessor e o seu sucessor.
+
+numero1 = int(input('digite um numero inteiro'))
+
+antecessor = numero1 - 1
+sucessor = numero1 + 1
+
+print(f'o seu antecessor sera: {antecessor}\t E o sucessor sera : {sucessor}')
+
+#ou 
+
+numero =  int(input('digite um numero inteiro  '))
+
+print(f'sucessor : {numero+1}')
+print(f'antecessor: {numero-1}')
+#----------------------------------------------------------------------------------------------------------------------------
+
+#Faça um programa que leia o nome o endereço e o telefone de
+#  um cliente e ao final, imprima esses dados.
+nome = input('digite o seu nome:')
+print(nome)
+endereco = input('digite seu indereço: ') 
+print(endereco)
+telefone = input('disce o seu numero de telefone: ')
+print(telefone)
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+#8. Faça um programa que leia dois números inteiros e imprima a
+#   subtração deles.
+
+numero1 = int(input('digite um numero inteiro:'))
+numero2 = int(input('digite outro numero inteiro: '))
+
+subtracao = numero1 + numero2
+
+print(f'o resultado sera {subtracao}')
